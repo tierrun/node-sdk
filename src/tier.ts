054f927445bb5edc7cb20df57407a5b3e4d6518d
isaacs <i@izs.me>
2022-04-30 10:38:33 -0700

    options and usage

--- src/tier.ts
+++ src/tier.ts
@@ -54,7 +54,48 @@ const usage = (msg: string, er?: any) => {
 }
 
 const topUsage = (er?: any) =>
-  usage(`tier: usage: tier [push|pull|help|login|logout]`, er)
+  usage(`tier: usage: tier [options] <command>
+
+Options:
+
+  --api=<url>      Set the tier API server base url.
+                   Defaults to TIER_API_URL env, or https://api.tier.run/
+
+  --web=<url>      Set the tier web server base url to use for login.
+                   Defaults to TIER_WEB_URL env, or https://tier.run/
+
+  --key=<token>    Specify the auth token for Tier to use.
+                   Tokens can be generated manually by visiting
+                   <https://tier.run/app/account/tokens>, minted for a project
+                   by running 'tier login', or set in the environment variable
+                   TIER_KEY.
+
+  --auth-type=<basic|bearer>
+                   Tell Tier to use the specified auth type.  Default: basic
+
+  --debug -d       Turn debug logging on
+  --no-debug -D    Turn debug logging off
+
+  --help -h        Show this usage screen.
+
+Commands:
+
+  login            Log in the CLI by authorizing in your web browser.
+                   Tokens are stored scoped to each project working directory
+                   and API server, and will not be active outside of that
+                   environment.
+
+  logout           Remove a login token from your system.
+
+  projectDir       Show the current project directory that login tokens are
+                   scoped to.
+
+  push <jsonfile>  Push the pricing model defined in <jsonfile> to Tier.
+
+  pull             Show the current model.
+
+  whoami           Get the organization ID associated with the current login.
+`, er)
 
 const projectRootFiles = [
   '.git',
@@ -89,52 +130,113 @@ const projectDir = (
   return projectDir(top, dn, start || dir) || start || dir
 }
 
+import {getOpt} from './getopt'
+
+const consumeOptions = (argv: string[]):void => {
+  const options = new Set(['api', 'web', 'key', 'auth-type'])
+  const switches = new Set(['debug', 'no-debug', 'd', 'D', 'help', 'H', 'h', '?'])
+
+  for (const [key, val] of getOpt(argv, options, switches)) {
+    switch (key) {
+      case 'debug':
+      case 'd':
+        process.env.TIER_DEBUG = '1'
+        continue
+
+      case 'no-debug':
+      case 'D':
+        process.env.TIER_DEBUG = '0'
+        continue
+
+      case 'help':
+      case 'h':
+      case 'H':
+      case '?':
+        topUsage()
+        return
+
+      case 'api':
+        process.env.TIER_API_URL = val
+        continue
+
+      case 'web':
+        process.env.TIER_WEB_URL = val
+        continue
+
+      case 'key':
+        process.env.TIER_KEY = val
+        continue
+
+      case 'auth-type':
+        process.env.TIER_AUTH_TYPE = val
+        continue
+
+      default:
+        topUsage(`unknown option: ${key}`)
+        return
+    }
+  }
+}
+
 const main = async (argv: string[]) => {
-  if (!argv[2]) {
+  argv.shift()
+  argv.shift()
+
+  consumeOptions(argv)
+
+  const cmd = argv.shift()
+
+  if (!cmd) {
     return topUsage()
   }
 
-  if (argv[2] === 'login') {
-    return doLogin()
+  if (cmd === 'login') {
+    return doLogin(argv)
   }
 
-  if (argv[2] === 'logout') {
-    return doLogout()
+  if (cmd === 'logout') {
+    return doLogout(argv)
   }
 
-  if (argv[2] === 'projectDir') {
-    console.log(projectDir())
-    process.exit(0)
+  if (cmd === 'projectDir') {
+    return showProjectDir(argv)
   }
 
+  switch (cmd) {
+    case 'push':
+      return doPush(argv)
+    case 'pull':
+      return doPull(argv)
+    case 'whoami':
+      return whoami(argv)
+    default:
+      return topUsage(`Unrecognized option or command: ${cmd}`)
+  }
+}
+
+const getClient = (): TierClient => {
   // if it's not a login request, we need to log in
-  let tc
   try {
-    tc = TierClient.fromEnv()
+    return TierClient.fromEnv()
   } catch (er) {
     try {
       // TODO: walk up until we find indications of a project, but no higher
       // than process.env.HOME
-      tc = TierClient.fromCwd(projectDir() || process.cwd())
+      return TierClient.fromCwd(projectDir() || process.cwd())
     } catch (er) {
       console.error(er)
       process.exit(1)
     }
   }
-
-  switch (argv[2]) {
-    case 'push':
-      return doPush(tc, argv[3])
-    case 'pull':
-      return doPull(tc)
-    case 'whoami':
-      return whoami(tc)
-    default:
-      return topUsage()
-  }
 }
 
-const doLogin = async (): Promise<void> => {
+const showProjectDir = (argv: string[]) => {
+  consumeOptions(argv)
+  console.log(projectDir())
+}
+
+const doLogin = async (argv: string[]): Promise<void> => {
+  consumeOptions(argv)
   const { default: opener } = await import('opener')
   const cwd = projectDir() || process.cwd()
   const tc = TierClient.fromEnv({ tierKey: TierClient.NO_AUTH })
@@ -166,35 +268,42 @@ Waiting...`)
   console.log(`Logged into tier!\nproject: ${cwd}`)
 }
 
-const doLogout = (): void => {
+const doLogout = (argv: string[]): void => {
+  consumeOptions(argv)
   const cwd = projectDir() || process.cwd()
   TierClient.fromEnv({ tierKey: TierClient.NO_AUTH }).logout(cwd)
 }
 
 const pushUsage = (er?: any) => usage(`usage: tier push <pricing.json>`, er)
 
-const whoami = async (tc: TierClient): Promise<void> => {
-  console.log(await tc.ping())
+const whoami = async (argv: string[]): Promise<void> => {
+  consumeOptions(argv)
+  console.log(JSON.stringify(await getClient().ping(), null, 2))
 }
 
 const pullUsage = (er?: any) => usage(`usage: tier pull`, er)
-const doPull = async (tc: TierClient): Promise<void> => {
+const doPull = async (argv: string[]): Promise<void> => {
+  consumeOptions(argv)
   try {
-    console.log(JSON.stringify(await tc.pullModel(), null, 2))
+    const data = await getClient().pullModel()
+    console.log(JSON.stringify(data, null, 2))
   } catch (er) {
     pullUsage(er)
   }
 }
 
 const doPush = async (
-  tc: TierClient,
-  fname: string | undefined
+  argv: string[]
 ): Promise<void> => {
+  consumeOptions(argv)
+  const fname = argv.shift()
   if (!fname) {
     return pushUsage(new Error('must supply filename'))
   }
+  consumeOptions(argv)
   try {
-    console.log(await tc.pushModel(JSON.parse(readFileSync(fname, 'utf8'))))
+    const data = JSON.parse(readFileSync(fname, 'utf8'))
+    console.log(await getClient().pushModel(data))
   } catch (er) {
     pushUsage(er)
   }
